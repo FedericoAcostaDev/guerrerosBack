39ad59f6867812db46698f67bf3c67b77d7b1bac
Federico <fedeacos045@gmail.com>
2022-01-06 13:06:47 -0300

    modifyng CORS

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ app.use(express.json());
 
 const corsOptions = {
   origin: "*",
+  credentials: true,
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 app.use(cors(corsOptions));
