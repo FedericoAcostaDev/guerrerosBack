5a8ba890d53da93dcb92ea8293775b88b562357b
Santiago Barreto <santiagonicolasbarreto@gmail.com>
2023-07-07 16:58:01 -0300

    add: user and auth crtls

--- src/users/user.routes.js
+++ src/users/user.routes.js
@@ -1,95 +1,13 @@
-import express from 'express'
-import User from './entities/user.entity.js'
-import Post from '../posts/entities/post.entity.js'
-import bcrypt from 'bcrypt'
-import cloudinary from '../shared/utils/cloudinary.js'
+import { Router } from 'express'
+import userCtrl from './controllers/user.controller.js'
 import upload from '../shared/utils/multer.js'
 
-const router = express.Router()
+const router = Router()
 
-// UPDATE
-router.put('/:id', upload.single('image'), async (req, res) => {
-  if (req.body.userId === req.params.id) {
-    if (req.body.password) {
-      const salt = await bcrypt.genSalt(10)
-      req.body.password = await bcrypt.hash(req.body.password, salt)
-    }
-    try {
-      // delete image from cloudinary
-      await cloudinary.uploader.destroy(user.cloudinary_id)
-      // upload image to cloudinary
-      const result = await cloudinary.uploader.upload(req.file.path)
-      const updatedUser = await User.findByIdAndUpdate(
-        req.params.id,
-        {
-          $set: req.body,
-          cloudinary_id: result.public_id
-        },
-        { new: true }
-      )
-
-      res.status(200).json(updatedUser)
-    } catch (err) {
-      res.status(500).json(err)
-    }
-  } else {
-    res.status(401).json('You can update only your account!')
-  }
-})
-
-// DELETE
-router.delete('/:id', async (req, res) => {
-  if (req.body.userId === req.params.id) {
-    try {
-      const user = await User.findById(req.params.id)
-      try {
-        await Post.deleteMany({ username: user.username })
-        await User.findByIdAndDelete(req.params.id)
-        // delete image from cloudinary
-        await cloudinary.uploader.destroy(user.cloudinary_id)
-        res.status(200).json('User has been deleted...')
-      } catch (err) {
-        res.status(500).json(err)
-      }
-    } catch (err) {
-      res.status(404).json('User not found!')
-    }
-  } else {
-    res.status(401).json('You can delete only your account!')
-  }
-})
-
-// GET USER
-router.get('/:id', async (req, res) => {
-  try {
-    const user = await User.findById(req.params.id)
-    const { password, ...others } = user._doc
-    res.status(200).json(others)
-  } catch (err) {
-    res.status(500).json(err)
-  }
-})
-
-// POST IMAGES
-router.post('/upload', upload.single('avatar'), async (req, res) => {
-  console.log('/users/upload triggered')
-  try {
-    // Upload image to cloudinary
-    const result = await cloudinary.uploader.upload(req.file.path)
-    // Save user
-    User.findById(req.body.id, (err, user) => {
-      if (err) console.log(err)
-      user.profilePic = result.secure_url
-      user.save((err) => {
-        if (err) console.log(err)
-        res.json(user)
-        console.log('user saved')
-      })
-    })
-  } catch (err) {
-    console.log(err)
-    res.status(500).json(err)
-  }
-})
+router
+  .get('/upload', upload.single('avatar'), userCtrl.uploadImage)
+  .get('/:id', userCtrl.getUser)
+  .put('/:id', userCtrl.updateUser)
+  .delete('/:id', userCtrl.deleteUser)
 
 export default router
