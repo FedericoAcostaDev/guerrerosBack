9d5a3aa0f409457271c8f063ad95044b27314ff4
Dc Dev Rep <69812733+DcDevRep@users.noreply.github.com>
2023-07-11 12:55:09 -0500

    act arch

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,5 +1,9 @@
 export const corsOptions = {
   origin: '*',
   credentials: true,
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  acceptControlAllowMethods: 'GET, POST, PUT, DELETE, OPTIONS',
+  acceptControlAllowHeaders: 'Origin, Content-Type, Accept',
+  acceptControlAllowOrigin: '*',
+  acceptControlAllowCredentials: true
 }
