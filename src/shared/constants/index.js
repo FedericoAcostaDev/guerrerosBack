17e2b7494edc3f677b9f788872c1114365767478
Santiago Barreto <santiagonicolasbarreto@gmail.com>
2023-07-11 02:37:36 -0300

    add: httpstatuses and error messages

---
+++ src/shared/constants/index.js
@@ -0,0 +1,22 @@
+export const HTTP_STATUSES = {
+  OK: 200,
+  CREATED: 201,
+  ACCEPTED: 202,
+  BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
+  FORBIDDEN: 403,
+  NOT_FOUND: 404,
+  INTERNAL_SERVER_ERROR: 500,
+  SERVICE_UNAVAILABLE: 503
+}
+
+export const ERROR_MESSAGES = {
+  INTERNAL_SERVER_ERROR: 'Internal Server Error',
+  RESOURCE_NOT_FOUND: 'Resource not found',
+  UNAUTHORIZED: 'Unauthorized',
+  FORBIDDEN: 'Forbidden',
+  BAD_REQUEST: 'Bad Request',
+  MISSING_PARAMETERS: 'Missing parameters',
+  INVALID_INPUT: 'Invalid input',
+  SERVICE_UNAVAILABLE: 'Servuce unavailable'
+}
