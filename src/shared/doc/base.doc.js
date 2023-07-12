a4719147698815960d2e05cbbb33b8c0911b6452
Dc Dev Rep <69812733+DcDevRep@users.noreply.github.com>
2023-07-11 19:24:21 -0500

    act arch

--- src/shared/doc/base.doc.js
+++ src/shared/doc/base.doc.js
@@ -9,6 +9,15 @@
  * @swagger
  * components:
  *  schemas:
+ *    ApiResponse:
+ *      type: object
+ *      properties:
+ *        code:
+ *          type: string
+ *        type:
+ *          type: string
+ *        message:
+ *          type: string
  *    NotFound:
  *      type: object
  *      properties:
@@ -25,6 +34,19 @@
  *          description: Error in the server
  *      example:
  *        msg: Error in the server
+ *  securitySchemes:
+ *    petstore_auth:
+ *      type: oauth2
+ *      flows:
+ *        implicit:
+ *          authorizationUrl: https://petstore3.swagger.io/oauth/authorize
+ *          scopes:
+ *            write:pets: modify pets in your account
+ *            read:pets: read your pets
+ *    api_key:
+ *      type: apiKey
+ *      name: api_key
+ *      in: header
  *
  *  parameters:
  *      Id:
