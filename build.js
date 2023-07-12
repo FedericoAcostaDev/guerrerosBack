e01288cdf128e86d8a11be9c08c07b8c9669e94f
Dc Dev Rep <69812733+DcDevRep@users.noreply.github.com>
2023-07-12 00:01:41 -0500

    act arch

--- build.js
+++ build.js
@@ -1,5 +1,11 @@
 import fs from 'fs'
-fs.writeFile('built-time.js', `module.exports = '${new Date()}'`, (err) => {
-  if (err) throw err
+
+const buildTime = new Date().toString()
+const filePath = 'built-time.js'
+
+fs.writeFile(filePath, `exports = '${buildTime}'`, (err) => {
+  if (err) {
+    throw err
+  }
   console.log('Build time file created successfully!')
 })
