c17cb0df6ecfa06fcd0755ddce3fad88867d7387
Santiago Barreto <santiagonicolasbarreto@gmail.com>
2023-07-22 15:54:08 -0300

    update: jwt utils

--- src/users/utils/jwt.js
+++ src/users/utils/jwt.js
@@ -3,16 +3,16 @@ import { Err } from '../../shared/helpers/error.js'
 import config from '../../config/index.js'
 const { SECRET_KEY } = config
 
-const generateToken = (payload) => jwt.sign(payload, SECRET_KEY, { expiresIn: 60 * 60 })
+const generateToken = (payload) => jwt.sign(payload, SECRET_KEY, { expiresIn: 60 })
 
 const verifyToken = (token) => {
   return new Promise((resolve, reject) => {
-    jwt.verify(token, SECRET_KEY, (err, user) => {
-      if (err) {
-        reject(new Err('Token not valid. Unauthorized', 403))
-      }
+    jwt.verify(token, SECRET_KEY, (err, decoded) => {
+      if (err && err?.message === 'jwt expired') reject(new Err('Token expired', 403))
+      if (err) reject(new Err('Token not valid. Unauthorized', 403))
+      // you need to implement verification token renewal both in the back and in the front
 
-      resolve(user)
+      resolve(decoded)
     })
   })
 }
